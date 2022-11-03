888eb18fd04677a1b2077f65dcc2459d2b742ae5
andi <andi@asillywalk.de>
2022-11-03 01:15:53 +0100

    [feat] Improved typings (generics) for Responder

--- src/Responder/Responder.php
+++ src/Responder/Responder.php
@@ -4,9 +4,14 @@ namespace Sillynet\Adretto\Responder;
 
 use Sillynet\Adretto\Action\Action;
 
+/**
+ * @phpstan-template A of Action
+ * @template A of Action
+ */
 interface Responder
 {
     /**
+     * @param A $action
      * @return void|null|mixed
      */
     public function respond(Action $action);
