f405c8760ead7d292793f8ed510c829b37a8836e
andi <andi@asillywalk.de>
2022-11-13 14:29:19 +0100

    [feat] Add REST module incorporating gebruederheitz/wp-simple-rest in principle

---
+++ src/Modules/REST/Action/RestAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sillynet\Adretto\Modules\REST\Action;
+
+use Sillynet\Adretto\Action\ActionHookAction;
+use Sillynet\Adretto\Action\InvokerWordpressHookAction;
+use Sillynet\Adretto\Modules\REST\RestRoute;
+
+abstract class RestAction extends InvokerWordpressHookAction implements
+    ActionHookAction
+{
+    public static string $restNamespaceBase = 'sillynet';
+    public static string $restApiVersion = '1';
+
+    /**
+     * This is where you set up your route: Create a new RestRoute object and
+     * configure it according to your needs – just don't set a callback, we'll
+     * handle that for you (it will be set to this object's "handle()" method).
+     *
+     * @return RestRoute
+     */
+    abstract protected function getRoute(): RestRoute;
+
+    /**
+     * This is the callback for your REST route.
+     *
+     * @return mixed
+     */
+    abstract protected function handle();
+
+    public function __invoke(...$args): void
+    {
+        $route = $this->getRoute();
+        $route->setCallback([$this, 'handle']);
+        register_rest_route(
+            self::getRestNamespace(),
+            $route->getPath(),
+            $route->getConfig(),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getWpHookName(): string
+    {
+        return 'rest_api_init';
+    }
+
+    public static function getRestNamespace(): string
+    {
+        return static::$restNamespaceBase . '/v' . static::$restApiVersion;
+    }
+}
---
+++ src/Modules/REST/RestRoute.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace Sillynet\Adretto\Modules\REST;
+
+/**
+ * @phpstan-type RestRouteArg array{description: string, default: ?mixed, type: ?string, validate_callback: ?callable, sanitize_callback: ?callable}
+ * @phpstan-type RestRouteConfig array{methods: string|array<string>, callback: ?callable, permission_callback: ?callable, args: array<string, RestRouteArg>}
+ */
+class RestRoute
+{
+    protected string $path = '';
+
+    /** @var RestRouteConfig  */
+    protected array $config = [
+        'methods' => 'GET',
+        'callback' => null,
+        'permission_callback' => null,
+        'args' => [],
+    ];
+
+    protected string $name = '';
+
+    public function __construct(string $name, string $path)
+    {
+        $this->name = $name;
+        $this->path = $path;
+    }
+
+    /**
+     * Builder method for fluent creation.
+     */
+    public static function create(string $name, string $path): self
+    {
+        return new RestRoute($name, $path);
+    }
+
+    /**
+     * @param string|array<string> $allowedMethods
+     *
+     * @return $this
+     */
+    public function setMethods($allowedMethods): self
+    {
+        $this->config['methods'] = $allowedMethods;
+
+        return $this;
+    }
+
+    public function setCallback(callable $callback): self
+    {
+        $this->config['callback'] = $callback;
+
+        return $this;
+    }
+
+    public function setPermissionCallback(callable $permissionCallback): self
+    {
+        $this->config['permission_callback'] = $permissionCallback;
+
+        return $this;
+    }
+
+    /**
+     * Set the permission_callback so only users with the 'edit_posts' capability
+     * can call this route.
+     *
+     * @return $this
+     */
+    public function allowOnlyEditors(): self
+    {
+        $this->config['permission_callback'] = function () {
+            return current_user_can('edit_posts');
+        };
+
+        return $this;
+    }
+
+    /**
+     * Set the permission_callback so only users with the 'install_plugins'
+     * capability can call this route.
+     *
+     * @return $this
+     */
+    public function allowOnlyAdmins(): self
+    {
+        $this->config['permission_callback'] = function () {
+            return current_user_can('install_plugins');
+        };
+
+        return $this;
+    }
+
+    /**
+     * Set the permission_callback so this route effectively becomes public,
+     * available to anyone.
+     *
+     * @return $this
+     */
+    public function allowAnyone(): self
+    {
+        $this->config['permission_callback'] = function () {
+            return true;
+        };
+
+        return $this;
+    }
+
+    /**
+     * @param ?mixed          $default
+     */
+    public function addArgument(
+        string $name,
+        string $description,
+        $default = null,
+        string $type = null,
+        callable $sanitizeCallback = null,
+        callable $validateCallback = null
+    ): self {
+        /** @var RestRouteArg $arg */
+        $arg = [
+            'description' => $description,
+        ];
+
+        if ($default !== null) {
+            $arg['default'] = $default;
+        }
+
+        if (!empty($type)) {
+            $arg['type'] = $type;
+        }
+
+        if ($sanitizeCallback !== null) {
+            $arg['sanitize_callback'] = $sanitizeCallback;
+        }
+
+        if ($validateCallback !== null) {
+            $arg['validate_callback'] = $validateCallback;
+        }
+
+        $this->config['args'][$name] = $arg;
+
+        return $this;
+    }
+
+    /**
+     * @param array<RestRouteArg> $args
+     */
+    public function addArguments(array $args): self
+    {
+        $this->config['args'] = array_merge($this->config['args'], $args);
+
+        return $this;
+    }
+
+    /**
+     * @param array<RestRouteArg> $args
+     */
+    public function replaceArguments(array $args): self
+    {
+        $this->config['args'] = $args;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return RestRouteConfig
+     */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @return array{name: string, route: string, config: RestRouteConfig}
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'route' => $this->path,
+            'config' => $this->config,
+        ];
+    }
+}
